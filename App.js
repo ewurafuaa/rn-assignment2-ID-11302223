a7d442510e7e69c304fd6b918cd250f18d8446c7
ewurafua <ewurafuaquansah@gmail.com>
2024-05-27 02:39:47 +0930

    bolded text

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My name is Ewurafua Quansah</Text>
+      <Text style={styles.text}>My name is <Text style={styles.boldText}>Ewurafua Quansah</Text></Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,5 +19,8 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 24,
+  },
+  boldText: {
+    fontWeight: 'bold',
   }
 });
